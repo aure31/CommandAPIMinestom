266c62610b4b676b8634696b0c05cfd1ac3bc5e7
kedor <99351777+aure31@users.noreply.github.com>
2024-10-02 17:05:30 +0200

    Delete commandapi-kotlin/commandapi-core-kotlin directory

--- commandapi-kotlin/commandapi-core-kotlin/src/main/kotlin/dev/jorel/commandapi/kotlindsl/CommandAPIDSL.kt
+++
@@ -1,7 +0,0 @@
-package dev.jorel.commandapi.kotlindsl
-
-import dev.jorel.commandapi.executors.CommandArguments
-import kotlin.reflect.KProperty
-
-// CommandArguments DSL
-inline operator fun <reified T> CommandArguments.getValue(nothing: Nothing?, property: KProperty<*>) = this[property.name] as T
